d2d956b077dd204e09c14b654f2c61755c590216
Tom Grennan <tgrennan@platinasystems.com>
2018-10-19 12:31:47 -0700

    purge sriov mode
    Signed-off-by: Tom Grennan <tgrennan@platinasystems.com>

--- platforms/mk1/platform.go
+++ platforms/mk1/platform.go
@@ -5,9 +5,7 @@
 package mk1
 
 import (
-	"github.com/platinasystems/go/goes/cmd/ip"
 	"github.com/platinasystems/go/internal/i2c"
-	"github.com/platinasystems/go/internal/sriovs"
 	"github.com/platinasystems/vnet"
 	"github.com/platinasystems/vnet/devices/bus/pci"
 	"github.com/platinasystems/vnet/devices/ethernet/ixge"
@@ -26,66 +24,6 @@ import (
 	"os"
 	"time"
 )
-
-func newSriovs(ver uint) error {
-	if ver > 0 {
-		sriovs.VfName = func(port, subport uint) string {
-			return fmt.Sprintf("eth-%d-%d", port+1, subport+1)
-		}
-	}
-	eth0, err := net.InterfaceByName("eth0")
-	if err != nil {
-		return err
-	}
-	mac := sriovs.Mac(eth0.HardwareAddr)
-	// skip over eth0, eth1, and eth2
-	mac.Plus(3)
-	sriovs.VfMac = mac.VfMac
-	return sriovs.New(vfs)
-}
-
-func delSriovs() error { return sriovs.Del(vfs) }
-
-func vlan_for_port(port, subport sriovs.Vf) (vf sriovs.Vf) {
-	// physical port number for data ports are numbered starting at 1.
-	// (phys 0 is cpu port...)
-	phys := sriovs.Vf(1)
-
-	// 4 sub-ports per port; mk1 ports are even/odd swapped.
-	phys += 4 * (port ^ 1)
-
-	phys += subport
-
-	// Vlan is 1 plus physical port number.
-	return sriovs.Vf(1 + phys)
-}
-
-// The vfs table is 0 based and is adjusted to 1 based beta and production
-// units with VfName
-var vfs = make_vfs()
-
-func make_vfs() [][]sriovs.Vf {
-	// pf0 = fe1 pipes 0 & 1; only 32 vfs.
-	// pf1 = fe1 pipes 2 & 3; only 32 vfs.
-	const (
-		n_port        = 32
-		n_sub_port    = 1
-		n_pf          = 2
-		n_port_per_pf = n_port / n_pf
-	)
-	var pfs [n_pf][n_port / n_pf]sriovs.Vf
-	for port := sriovs.Vf(0); port < n_port; port++ {
-		for subport := sriovs.Vf(0); subport < n_sub_port; subport++ {
-			vf := port<<sriovs.PortShift | subport<<sriovs.SubPortShift | vlan_for_port(port, subport)
-			pf := port / n_port_per_pf
-			i := port%n_port_per_pf + n_port_per_pf*subport
-			if i < sriovs.Vf(len(pfs[pf])) {
-				pfs[pf][i] = vf
-			}
-		}
-	}
-	return [][]sriovs.Vf{pfs[0][:], pfs[1][:]}
-}
 
 func PlatformInit(v *vnet.Vnet, p *fe1_platform.Platform) (err error) {
 	if !p.KernelIxgbe {
@@ -135,21 +73,8 @@ func PlatformInit(v *vnet.Vnet, p *fe1_platform.Platform) (err error) {
 	return
 }
 
-func PlatformExit(v *vnet.Vnet, p *fe1_platform.Platform) (err error) {
-	if !p.KernelIxgbe {
-		return
-	}
-	if !p.KernelIxgbevf {
-		// FIXME should we delete eth-PORT-SUBPORT vlan
-		// interfaces or leave them up to continue forwarding
-		// until vnet reloads?
-	} else {
-		err = delSriovs()
-	}
-	if xerr := ip.Goes.Main("link", "delete", "vnet"); err == nil {
-		err = xerr
-	}
-	return
+func PlatformExit(v *vnet.Vnet, p *fe1_platform.Platform) error {
+	return nil
 }
 
 type pca9535_main struct {
